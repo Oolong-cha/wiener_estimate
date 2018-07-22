bcd402be712febbf47d27e0d72d048b8895632fb
Oolong-cha <matsui.f.aa@m.titech.ac.jp>
2018-07-22 12:42:29 +0900

    要素を中心座標に割り当てる

--- window_function.m
+++ window_function.m
@@ -1,8 +1,9 @@
-function [blockmat,rgb] = window_function(M_estmatrix,N1,N2)
-
+function [blockmat,rgb] = window_function
+% window_function(M_estmatrix,N1,N2,bpl)
 %----変数-----
 %ブロックごとに割り当てられている推定行列:M_estmatrix
 %高解像度RGB画像の高さ幅:N1,N2
+%1ブロックごとのピクセル長:bpl
 %rgb画像:grgb
 %分光画像:sp81
 %低解像度分光画像のいちピクセルの一辺の長さ:L 例えば2なら解像度が半分になる
@@ -13,23 +14,32 @@ function [blockmat,rgb] = window_function(M_estmatrix,N1,N2)
 %推定行列と座標行列で分けたらメモリへらせる
 %ブロックごとに異なる推定行列が割り当てられている
 %高解像度RGBを基準にして、中心である右下のピクセルに割り当てる
-blockmat=
-
 %ブロックの長さ
-bpl=2;
+bpl=4;
+N1=20;
+N2=20;
+cnt=0;
+blockmat=zeros(N1,N2)
+for i=(bpl/2)+1:bpl:((N1/bpl)-1)*bpl+((bpl/2)+1)
+    for j=(bpl/2)+1:bpl:((N2/bpl)-1)*bpl+((bpl/2)+1)
+        cnt=cnt+1;
+        blockmat(i,j)=cnt;
+    end
+end
+
 
 %ホントは二重ループやめたい
 %ブロックの配列　5*5
 %ブロックごとに異なる推定行列が割り当てられている
 %高解像度RGBを基準にして、中心である右下のピクセルに割り当てる
-blockmat=zeros(10,10);
-cnt=0;
-for i=2:2:10
-    for j=2:2:10
-        cnt=cnt+1;
-        blockmat(i,j)=cnt;
-    end
-end
+% blockmat=zeros(10,10);
+% cnt=0;
+% for i=2:2:10
+%     for j=2:2:10
+%         cnt=cnt+1;
+%         blockmat(i,j)=cnt;
+%     end
+% end
 %今回はベクトルじゃなくてスカラー値をわりふる
 % for i=1:25
 %     blockmat(1,i)=i;
