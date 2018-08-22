81c9ccb5bebd4b23a80afbbf8214801446b44fb1
Oolong-cha <matsui.f.aa@m.titech.ac.jp>
2018-08-22 15:09:59 +0900

    高速化

--- spec2rgb.m
+++ spec2rgb.m
@@ -1,13 +1,7 @@
 function [grgb,grgb_norm]=spec2rgb(sp81,ill81,rgb81,height,width)
 %引数:分光反射率、照明光、分光感度特性(rgb）,height,width
 
-%照明光と分光感度の積
-g=zeros(height*width,81);
-g=sp81.*ill81;
-
-%配列確保
-grgb=horzcat(sum(g.*rgb81(1,:),2),sum(g.*rgb81(2,:),2),sum(g.*rgb81(3,:),2));
-
+grgb=horzcat(sum(sp81.*ill81.*rgb81(1,:),2),sum(sp81.*ill81.*rgb81(2,:),2),sum(sp81.*ill81.*rgb81(3,:),2));
 
 %輝度正規化
 ref=ones(1,81); %全ての反射率が1の分光反射率を作製
@@ -35,8 +29,6 @@ grgb_norm(grgb_norm>1)=1.0;
 
 grgb=reshape(grgb,height,width,3);
 grgb_norm=reshape(grgb_norm,height,width,3);
-
-
 
 end
 
