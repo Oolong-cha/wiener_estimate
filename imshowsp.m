5350ac17ee59b6872ecff400f4f1b59279ab3395
Oolong-cha <matsui.f.aa@m.titech.ac.jp>
2018-10-23 16:37:29 +0900

    ピクセル情報表示

--- imshowsp.m
+++ imshowsp.m
@@ -1,8 +1,8 @@
 function imshowsp
-%     macbethsp=csvread('../data/macbeth.csv'); %マクベス分光反射率データ
-%     img=macbethchart(macbethsp,8,4); %分光画像作成
-    img=csvread('C:\Users\fumin\Documents\wiener_estimate\w_4-1.csv');
-    img=reshape(img,1024,1280,81);
+    macbethsp=csvread('../data/macbeth.csv'); %マクベス分光反射率データ
+    img=macbethchart(macbethsp,8,4); %分光画像作成
+%     img=csvread('C:\Users\fumin\Documents\wiener_estimate\w_4-1.csv');
+%     img=reshape(img,1024,1280,81);
     % Create a figure and axes
     f = figure('Visible','off');
     ax = axes('Units','pixels');
@@ -12,6 +12,8 @@ function imshowsp
     S = S_a;
     sno = sno_a;
     imshow(img(:,:,S));
+    hp = impixelinfo;
+set(hp,'Position',[5 1 300 20]);
 
     sld = uicontrol('Style', 'slider',...
         'Min',1,'Max',sno_a,'Value',S_a,'SliderStep',[1/(sno_a-1) 10/(sno_a-1)],...
@@ -32,6 +34,9 @@ function imshowsp
         S = round(get(hObj,'Value')); 
         imshow(img(:,:,S));
         set(stxthand, 'String', sprintf('Slice# %d / %d',S, sno)) 
+        hp = impixelinfo;
+        set(hp,'Position',[5 1 300 20]);
+
     end
 
 
